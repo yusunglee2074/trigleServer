6d99991e7c3b8a563d77ee55cc3781f9ae9913b0
lee yusung <yusunglee2074@lees-MacBook-Pro.local>
2018-12-06 13:49:19 +0900

    add address query and mutation

--- schema/Address.js
+++ schema/Address.js
@@ -2,16 +2,30 @@ module.exports = { types: `
   type Address {
     id: Int!
     receiverName: String
-    receiverId: User
+    receiverId: ID
     address1: String
     address2: String
     detailAddress: String
-    profileImage: Media
+    profileImage: ID
     numberOfSent: Int
     numberOfReceived: Int
-  }`,
+  }
+  input addressInput {
+    receiverName: String
+    receiverId: String
+    address1: String
+    address2: String
+    detailAddress: String
+    profileImage: String
+  }
+  `,
   queries: `
+  getAddresses: [Address]
+  getAddress(id: Int!): Address
   `,
   mutations: `
+  createAddress(input: addressInput, id: Int): Address
+  updateAddress(input: addressInput): Address
+  deleteAddress(id: Int!): Boolean
   `
 }
